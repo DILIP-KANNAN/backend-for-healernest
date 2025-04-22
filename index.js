968a77a3d470390a59d91da00c3b087f2be0519d
DILIP-KANNAN <231501040@rajalakshmi.edu.in>
2025-04-22 05:37:23 +0530

    next

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // ✅ Only use admin SDK here
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY).replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
